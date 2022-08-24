2e495f8649597495eaf4d0ec9165665978a4b36f
van <vancednguyen@gmail.com>
2022-08-23 17:47:21 -0700

    yes

---
+++ world/src/main/java/hellow/App.java
@@ -0,0 +1,17 @@
+package hellow;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        for ( int i = 0; i < 10; i++) {
+            System.out.println(i + i);
+            System.out.println("yes");
+        }
+    }
+}
