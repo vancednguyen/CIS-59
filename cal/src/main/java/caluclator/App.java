b5e5b96823c924005cd75081c93cb61ac8c4739a
van <vancednguyen@gmail.com>
2022-09-04 11:34:27 -0700

    Updating calculator to coincide with the rubric
    This update will increase readability with improved names and consistency

--- cal/src/main/java/caluclator/App.java
+++ cal/src/main/java/caluclator/App.java
@@ -2,127 +2,121 @@ package caluclator;
 
 /** Vance Nguyen
  * CIS-059 OOP
- * 08/30/2022
+ * 09/04/2022
  */
 import java.util.Scanner;
 
 public class App 
 {
-    static Scanner boi;
+    static Scanner scanning;
 
     public static void main(String[] args) {
-        float numba = 0.000f;
-        float numbb = 0.000f;
-        boi = new Scanner(System.in);
+        float numberA = 0.000f;
+        float numberB = 0.000f;
+        scanning = new Scanner(System.in);
         String select = " ";
         try {
             do {
-                select = displaycal(numba, numbb);
+                select = displayCal(numberA, numberB);
                 switch (select) {
                     case "a":
-                        numba = SetA();
+                        numberA = setA();
                         break;
                     case "b":
-                        numbb = SetB();
+                        numberB = setB();
                         break;
                     case "+":
-                        numba = add(numba, numbb);
+                        numberA = add(numberA, numberB);
                         break;
                     case "-":
-                        numba = subtract(numba, numbb);
+                        numberA = subtract(numberA, numberB);
                         break;
                     case "*":
-                        numba = mulitply(numba, numbb);
+                        numberA = multiply(numberA, numberB);
                         break;
                     case "/":
-                        numba = divide(numba, numbb);
+                        numberA = divide(numberA, numberB);
                         break;
                     case "c":
-                        numba = 0.00f;
-                        numbb = 0.00f;
+                        numberA = 0.000f;
+                        numberB = 0.000f;
                         break;
                     case "q":
                         break;
                     default:
-                        sop("Illegal input");
+                        System.out.println("Illegal input");
                 }
             } while (!select.equals("q"));
         } catch (Exception e) {
-            sop("Something went wrong");
+            System.out.println("Something went wrong");
             e.printStackTrace();
         }
-       boi.close();
+       scanning.close();
     }
 
-    public static void sop(Object x) {
-        System.out.println(x);
-    }
-    public static void sopr(Object x) {
-        System.out.print(x);
-    }
 
-    public static String displaycal(float numba, float numbb) {
+    public static String displayCal(float numberA, float numberB) {
         String command = "";
-        sop("-----------------------------");
-        sop("Chavvi Calc");
-        sop("-----------------------------");
-        System.out.printf("A = %.3f \t", numba);
-        System.out.printf("B = %.3f \n", numbb);
-        sop("-----------------------------");
-        sop("a \t Enter a value for A");
-        sop("b \t Enter b value for B");
-        sop("+ \t Add ");
-        sop("- \t Subtract");
-        sop("* \t Multiply");
-        sop("/ \t Divide");
-        sop("c \t Clear");
-        sop("q \t Quit");
-        sop("-----------------------------");
-        sopr("Enter a command ");
-        command = boi.nextLine().toLowerCase();
+        System.out.println("-----------------------------");
+        System.out.println("Chavvi Calc");
+        System.out.println("-----------------------------");
+        System.out.printf("A = %.3f \t", numberA);
+        System.out.printf("B = %.3f \n", numberB);
+        System.out.println("-----------------------------");
+        System.out.println("a \t Enter a value for A");
+        System.out.println("b \t Enter b value for B");
+        System.out.println("+ \t Add ");
+        System.out.println("- \t Subtract");
+        System.out.println("* \t Multiply");
+        System.out.println("/ \t Divide");
+        System.out.println("c \t Clear");
+        System.out.println("q \t Quit");
+        System.out.println("-----------------------------");
+        System.out.print("Enter a command ");
+        command = scanning.nextLine().toLowerCase();
         return command;
 
     }
 
-    static float SetA() {
+    static Float setA() {
         try{
-            float number = 0;
-            sopr("Enter a number ");
-            number = boi.nextFloat();
-            boi.nextLine();
+            Float number = 000f;
+            System.out.print("Enter a number ");
+            number = scanning.nextFloat();
+            scanning.nextLine();
             return number;
         }catch(Exception e) {
-            sop("Error: Enter a number next time");
-            boi.nextLine();
-            return 0;
+            System.out.println("Error: Enter a number next time");
+            scanning.nextLine();
+            return 0.000f;
         }
     }
-    static float SetB() {
+    static Float setB() {
         try {
-            float number = 0;
-            sopr("Enter a number ");
-            number = boi.nextFloat();
-            boi.nextLine();
+            Float number = 000f;
+            System.out.print("Enter a number ");
+            number = scanning.nextFloat();
+            scanning.nextLine();
             return number;
         } catch (Exception e) {
-            sop("Error: Enter a number next time");
-            boi.nextLine();
-            return 0;
+            System.out.println("Error: Enter a number next time");
+            scanning.nextLine();
+            return 0.000f;
         }
     }
-    static float add(Float x, Float y) {
+    static Float add(Float x, Float y) {
         return x + y;
     }
-    static float subtract(float x, float y) {
+    static Float subtract(Float x, Float y) {
         return x - y;
     }
-    static float mulitply(float x, float y) {
+    static Float multiply(Float x, Float y) {
         return x * y;
     }
-    static float divide(float x, float y) {
+    static Float divide(Float x, Float y) {
         if (x == 0 || y ==0){
-            sop("Error: can't divide 0");
-            return 0;
+            System.out.println("Error: can't divide 0");
+            return 0.000f;
         }
         else {
             return x / y;
